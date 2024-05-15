1f2d7bb5f3e72b6bafa1347d4dfeae39879e030b
Eric Seidler <eric.seidler@gmail.com>
2024-05-15 09:02:05 -0500

    more starter code updates

--- lesson-3-cloud-automation/exercises/starter/exercise-3/_config.tf
+++ lesson-3-cloud-automation/exercises/starter/exercise-3/_config.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "udacity-tf-emmanuel-student" # Update here with your S3 bucket
+    bucket = "udacity-tf-seidler" # Update here with your S3 bucket
     key    = "terraform/lesson3-ex-3.tfstate"
     region = "us-east-2"
   }
